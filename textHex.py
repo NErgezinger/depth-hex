99de23a64a9df527445f348e0659e22ac89ae41f
ergezinger <noah.ergezinger@gmail.com>
2016-07-25 10:16:19 -0600

    Update textHex.py

--- textHex.py
+++ textHex.py
@@ -1,3 +1,4 @@
+version = 2.4
 import random
 import time
 from copy import deepcopy
@@ -6,11 +7,12 @@ from copy import deepcopy
 board = []
 gameWon = False
 hexChar = "*"
-compChar = "B"
-playerChar = "W"
+compChar = "b"
+playerChar = "w"
 turn = "player"
-size = 4
-duration = .5
+size = 3
+duration = 5
+cmdQuit = False
 adjListo = [[0,1], #upRight
            [1,0], #right
            [1,-1],#downRight
@@ -26,18 +28,23 @@ adjList = [[1,1], #upRight
            [0,1]]#upLeft
 
 #board initialization
-for i in range(size):
-    row = []
-    for j in range(size):
-        row.append(hexChar)
-    board.append(row)
+def resetBoard(b):
+    b = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(hexChar)
+        b.append(row)
+    return b
+        
+board = resetBoard(board)
 
 def printBoard(b):
     for i in range(size - 1, -1, -1):
         printRow = []
         for row in b:
             printRow.append(row[i])
-        indent = " " * (size - i)
+        indent = " " * ((size - i) - 1)
         print(indent + " ".join(printRow))
 
 def changeColour(c, x, y):
@@ -115,7 +122,7 @@ def compMove(c):
     count = 0
     for i in emptySpots:
         winResultsCount.append(0)
-    print("Starting search for:", duration, "seconds")
+##    print("Starting search for:", duration, "seconds")
     while startTime + duration > time.clock() or len(winResults) == 0:
         for spot in emptySpots:
             if startTime + duration < time.clock() and len(winResults) > 0: break
@@ -128,8 +135,8 @@ def compMove(c):
             count += 1
             
     best = winResults[winResultsCount.index(max(winResultsCount))]
-    print("Searched: ", count)
-    print("Chosen position won:", max(winResultsCount), "times")
+##    print("Searched: ", count)
+##    print("Chosen position won:", max(winResultsCount), "times")
     return best
 
 def simulateGame(simPlayer, startMove):
@@ -164,35 +171,133 @@ def simulateGame(simPlayer, startMove):
                 return True
             emptySpots = getSpots(hexChar, simBoard)
 
-    print("Simulation board is full, but no winner. This shouldn't happen")
+##    print("Simulation board is full, but no winner. This shouldn't happen")
     
 
-def main():
-    global turn
-    global gameWon
-    if turn == "player":
-        printBoard(board)
-        inputCoord = list(map(int, input("Enter coordinates: (x,y): ").split(".")))
-        while not board[inputCoord[0]-1][inputCoord[1]-1] == hexChar:
-            inputCoord = list(map(int, input("Enter coordinates: (x,y): ").split(".")))
-        changeColour(playerChar, inputCoord[0]-1, inputCoord[1]-1)
-##        moveCoord = compMove(playerChar)
-##        changeColour(playerChar, moveCoord[0], moveCoord[1])
-        printBoard(board)
-        if checkPlayerWon(board):
-            print("WHITE WINS!")
-            printBoard(board)
-            gameWon = True
-        turn = "comp"
-    elif turn == "comp":
-        moveCoord = compMove(compChar)
-        changeColour(compChar, moveCoord[0], moveCoord[1])
+##def main():
+##    global turn
+##    global gameWon
+##    if turn == "player":
 ##        printBoard(board)
-        if checkCompWon(board):
-            print("BLACK WINS!")
-            printBoard(board)
-            gameWon = True
-        turn = "player"   
+##        inputCoord = list(map(int, input("Enter coordinates: (x,y): ").split(".")))
+##        while not board[inputCoord[0]-1][inputCoord[1]-1] == hexChar:
+##            inputCoord = list(map(int, input("Enter coordinates: (x,y): ").split(".")))
+##        changeColour(playerChar, inputCoord[0]-1, inputCoord[1]-1)
+####        moveCoord = compMove(playerChar)
+####        changeColour(playerChar, moveCoord[0], moveCoord[1])
+##        printBoard(board)
+##        if checkPlayerWon(board):
+##            print("WHITE WINS!")
+##            printBoard(board)
+##            gameWon = True
+##        turn = "comp"
+##    elif turn == "comp":
+##        moveCoord = compMove(compChar)
+##        changeColour(compChar, moveCoord[0], moveCoord[1])
+####        printBoard(board)
+##        if checkCompWon(board):
+##            print("BLACK WINS!")
+##            printBoard(board)
+##            gameWon = True
+##        turn = "player"   
 
-while not gameWon:
-    main()
+def play(c, ac):
+    acl = [0,0]
+    acl[0] = ord(ac[0]) - 96
+    acl[1] = abs(int(ac[1:]) - size) + 1
+    changeColour(c, acl[0]-1, acl[1]-1)
+
+def convertMove(move):
+    move[0] = chr(move[0]+97)
+    move[1] = abs(move[1] - size)
+    move[1] = str(move[1])
+    s = "".join(move)
+    return s
+
+while not cmdQuit:
+    cmd = input().split(" ")
+    
+    if cmd[0] == "name":
+        print("= textHex")
+        print()
+        
+    elif cmd[0] == "version":
+##        print("=", str(version))
+        print("= 1")
+        print()
+    elif cmd[0] == "genmove":
+        if cmd[1] == "b" or cmd[1] == "black":
+            if not checkPlayerWon(board) and not checkCompWon(board):
+                move = compMove(compChar)
+                changeColour(compChar, move[0], move[1])
+                fmove = convertMove(move)
+                print("= ", fmove)
+                print()
+            else:
+                print("= resign")
+                print()
+        elif cmd[1] == "w" or cmd[1] == "white":
+            if not checkCompWon(board) and not checkPlayerWon(board):
+                move = compMove(playerChar)
+                changeColour(playerChar, move[0], move[1])
+                fmove = convertMove(move)
+                print("= ", fmove)
+                print()
+            else:
+                print("= resign")
+                print()
+        else:
+            print("? invalid color")
+
+    elif cmd[0] == "play":
+        if cmd[1] == "b" or cmd[1] == "black":
+            play(compChar, cmd[2])
+            print("= ")
+            print()
+        elif cmd[1] == "w" or cmd[1] == "white":
+            play(playerChar, cmd[2])
+            print("= ")
+            print()
+        else:
+            print("? invalid color")
+            print()
+            
+    elif cmd[0] == "undo":
+        print("= ")
+        print()
+
+    elif cmd[0] == "showboard":
+        print("= ")
+        printBoard(board)
+        print()
+
+    elif cmd[0] == "boardsize":
+        if cmd[1] == cmd[2]:
+            size = int(cmd[1])
+            board = resetBoard(board)
+            print("= ")
+            print()
+        else:
+            print("? incorrect board size")
+            print()
+
+    elif cmd[0] == "quit":
+        print("= ")
+        print()
+        cmdQuit = True
+
+    elif cmd[0] == "final_score":
+        if checkPlayerWon(board):
+            print("= W")
+            print()
+        elif checkCompWon(board):
+            print("= B")
+            print()
+        else:
+            print("=")
+            print()
+
+    elif cmd[0] == "hexgui-analyze_commands":
+        print("= ")
+        print()
+    
