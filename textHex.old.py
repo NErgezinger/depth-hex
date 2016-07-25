4ef5223a079d828e2f20f9537531889bb9ce7908
ergezinger <noah.ergezinger@gmail.com>
2016-07-25 10:13:32 -0600

    Update textHex.old.py

--- textHex.old.py
+++ textHex.old.py
@@ -1,168 +1,164 @@
 import random
-grid = []
-playerWon = False
-compWon = False
+import time
+from copy import deepcopy
+from statistics import mode
+import numpy
+import scipy.stats
+board = []
+gameWon = False
 hexChar = "□"
 compChar = "⬡"
 playerChar = "⬢"
-turnCurrent = "player"
+turn = "player"
 size = 3
-neighbors = [[0,1],     #upRight
-              [1,0],    #right
-              [1,-1],    #downRight
-              [0,-1],    #downLeft
-              [-1,0],    #left
-              [-1,1]]   #upLeft
+adjList = [[0,1], #upRight
+           [1,0], #right
+           [1,-1],#downRight
+           [0,-1],#downLeft
+           [-1,0],#left
+           [-1,1]]#upLeft
 
-smartNeighbors = [[1,-1],
-                  [-1,1]]
+vertAdjList = [[1,-1],
+               [-1,1]]
 
-
+#board initialization
 for i in range(size):
-    columns = []
+    row = []
     for j in range(size):
-        columns.append(hexChar)
-    grid.append(columns)
-    
-def changeColour(colour, x, y):
-    grid[x][y] = colour
+        row.append(hexChar)
+    board.append(row)
 
-def clamp(n, smallest, largest): return max(smallest, min(n, largest))
-def clampBoard(n): return max(0, min(n, size - 1))
+def printBoard(b):
+    for i in range(size - 1, -1, -1):
+        printRow = []
+        for row in b:
+            printRow.append(row[i])
+        indent = " " * i
+        print(indent + " ".join(printRow))
 
-def checkPlayerWon():
-    startList = []
-    for rowNumber, row in enumerate(grid):
-        if row[0] == playerChar:
-            startList.append(rowNumber)
-    for startRow in startList:
-        col = 0
-        prevRow = startRow
-        stillLooking = True
-        while stillLooking:
-            stillLooking = False
-            col += 1
-            if grid[clampBoard(prevRow + 1)][clampBoard(col - 1)] == playerChar and prevRow < size - 1:
-                col -= 1
-                prevRow += 1
-                stillLooking = True
-            elif grid[clampBoard(prevRow)][clampBoard(col)] == playerChar:
-                stillLooking = True
-            elif grid[clampBoard(prevRow - 1)][clampBoard(col)] == playerChar:
-                prevRow -= 1
-                stillLooking = True
-            if col >= size:
-                return True
-        return False
-            
-def checkCompWon():
-    startList = []
-    for colNumber, col in enumerate(grid[0]):
-        if col == compChar:
-            startList.append(colNumber)
-    for startCol in startList:
-        row = 0
-        prevCol = startCol
-        stillLooking = True
-        while stillLooking:
-            stillLooking = False
-            row += 1
-            if grid[clampBoard(row)][clampBoard(prevCol)] == playerChar:
-                stillLooking = True
-            elif grid[clampBoard(row)][clampBoard(prevCol - 1)] == playerChar:
-                prevCol -= 1
-                stillLooking = True
-            if row >= size:
-                return True
-        return False
+def changeColour(c, x, y):
+    board[x][y] = c
 
-def compMove():
+def getAdj(colour,x,y,b):
+    sameAdj = []
+    for n in adjList:
+        toCheck = [x + n[0], y + n[1]]
+        if 0 <= toCheck[0] <= size - 1 and 0 <= toCheck[1] <= size - 1:
+            if b[toCheck[0]][toCheck[1]] == colour:
+                sameAdj.append(toCheck)
+##    print("(" + str(x) + "," + str(y) + ") is adj to " + str(sameAdj))
+    return sameAdj
+
+def getSpots(c,b):
+    spots = []
+    for colNum,col in enumerate(b):
+        for rowNum,row in enumerate(col):
+            if row == c:
+                spots.append([colNum, rowNum])
+    return spots
+
+def checkPlayerWon(b):
+    searched = []
+    connectedToLeft = []
     playerSpots = []
+    rightEdge = []
+    for i in range(size):
+        rightEdge.append([size-1,i])
+    for rowNum,row in enumerate(b[0]):
+        if row == playerChar:
+            connectedToLeft.append([0,rowNum])
+    playerSpots = getSpots(playerChar, b)
+    for i in range(size * size):
+        for spot in playerSpots:
+            adjSpots = getAdj(playerChar, spot[0], spot[1], b)
+            for adj in adjSpots:
+                if adj in connectedToLeft and spot not in connectedToLeft:
+                    connectedToLeft.append(spot)
+    for spot in connectedToLeft:
+        if spot in rightEdge:
+            return True
+    return False
+
+def checkCompWon(b):
+    searched = []
+    connectedToBottom = []
     compSpots =[]
-    emptySpots = []
-    possibleMoves = []
-    for i, row in enumerate(grid):
-        for j, col in enumerate(row):
-            if col == playerChar:
-                playerSpots.append([i,j])
-            elif col == compChar:
-                compSpots.append([i,j])
-            elif col == hexChar:
-                emptySpots.append([i,j])
-    for spot in emptySpots:
-        for neighbor in smartNeighbors:
-            coordToCheck = [clampBoard(spot[1] + neighbor[0]), clampBoard(spot[0] + neighbor[1])]
-            spotToCheck = grid[coordToCheck[1]][coordToCheck[0]]
-            if spotToCheck == compChar and possibleMoves.count(spot) == 0:
-                possibleMoves.append(spot)
-    if len(possibleMoves) > 0:
-        return random.choice(possibleMoves)
-    else:
-        start = [int((size - 1) / 2) , int((size - 1) / 2)]
-        if grid[start[0]][start[1]] == hexChar:
-            return start
-        else:
-            rand = [random.randint(0,size - 1) , random.randint(0,size - 1)]
-            return rand
+    topEdge = []
+    for i in range(size):
+        topEdge.append([i,size-1])
+    for colNum,col in enumerate(b):
+        if col[0] == compChar:
+            connectedToBottom.append([colNum,0])
+    compSpots = getSpots(compChar, b)
+    for i in range(size * size):
+        for spot in compSpots:
+            adjSpots = getAdj(compChar, spot[0], spot[1], b)
+            for adj in adjSpots:
+                if adj in connectedToBottom and spot not in connectedToBottom:
+                    connectedToBottom.append(spot)
+    for spot in connectedToBottom:
+        if spot in topEdge:
+            return True
+    return False
     
-while not playerWon and not compWon:
-    if turnCurrent == "comp":
-        colour = compChar
-        tries = 0
-        while tries < 10:
-            compMoveXY = compMove()
-            compMoveX = compMoveXY[0]
-            compMoveY = compMoveXY[1]
-            tries += 1
-            if grid[compMoveX][compMoveY] == hexChar and tries <= 10:
-                changeColour(colour, compMoveX, compMoveY)
-                break
-            elif tries >= 10:
-                break
-                print("computer can't move")
-        if checkCompWon():
-            print("You Lose!")
-            compWon = True
-    elif turnCurrent == "player":
-        for i in range(size - 1, -1, -1):
-            indent = " " * i
-            print(indent + " ".join(grid[i]))
-        colour = playerChar
+def compMove():
+    emptySpots = []
+    winResult = []
+    emptySpots = getSpots(hexChar, board)
+    startTime = time.clock()
+    duration = 2
+    while startTime + duration > time.clock():
+        for spot in emptySpots:
+            outcome = simulateGame(compChar, spot)
+            if outcome:
+                winResult.append(spot)
+    common = scipy.stats.mode(winResult)[0]
+    common = common.astype(int)
+    common = common[0]
+    print(common)
+    return common
+
+def simulateGame(simPlayer, startMove):
+    simBoard = deepcopy(board)
+    simBoard[startMove[0]][startMove[1]] = simPlayer
+    emptySpots = getSpots(hexChar, simBoard)
+
+    while len(emptySpots) > 0:
+        rPlayerMove = random.choice(emptySpots)
+        simBoard[rPlayerMove[0]][rPlayerMove[1]] = playerChar
+        if checkPlayerWon(simBoard):
+            return False
+        emptySpots = getSpots(hexChar, simBoard)
+        if len(emptySpots) > 0:
+            rCompMove = random.choice(emptySpots)
+            simBoard[rCompMove[0]][rCompMove[1]] = compChar
+        if checkCompWon(simBoard):
+            return True
+        emptySpots = getSpots(hexChar, simBoard)
+
+    print("Simulation board is full, but no winner. This shouldn't happen")
+    
+
+def main():
+    global turn
+    if turn == "player":
+        printBoard(board)
         inputCoord = list(map(int, input("Enter coordinates: (x,y): ").split(".")))
-        while not grid[inputCoord[1]-1][inputCoord[0]-1] == hexChar:
+        while not board[inputCoord[0]-1][inputCoord[1]-1] == hexChar:
             inputCoord = list(map(int, input("Enter coordinates: (x,y): ").split(".")))
-        changeColour(colour, inputCoord[1]-1, inputCoord[0]-1)
-        if checkPlayerWon():
-            print("You win!")
-            playerWon = True
-    if turnCurrent == "player":
-        turnCurrent = "comp"
-    else:
-        turnCurrent = "player"
+        changeColour(playerChar, inputCoord[0]-1, inputCoord[1]-1)
+        if checkPlayerWon(board):
+            print("YOU WIN!")
+            gameWon = True
+        turn = "comp"
+    elif turn == "comp":
+        moveCoord = compMove()
+        print(moveCoord)
+        changeColour(compChar, moveCoord[0], moveCoord[1])
+        if checkCompWon(board):
+            print("YOU LOSE!")
+            gameWon = True
+        turn = "player"
 
-
-for i in range(size - 1, -1, -1):
-    indent = " " * i
-    print(indent + " ".join(grid[i]))
-
-##def oldCheckCompWon():
-##    startList = []
-##    for i, col in enumerate(grid[0]):
-##        if col == compChar:
-##            startList.append(i)
-##    col = 1
-##    for start in startList:
-##        while grid[clamp(col,0,size - 1)][start] == compChar:
-##            if col >= size - 1:
-##                return True
-##            col += 1
-##        col = 1
-##        while grid[col][start - 1] == compChar:
-##            if col >= size - 1:
-##                return True
-##            col += 1
-##        col = 1
-##        while grid[col][clamp(start + 1,0,size -1 )] == compChar:
-##            if col >= size - 1:
-##                return True
-##            col += 1
+while not gameWon:
+    main()
